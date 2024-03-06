1e646f5608f7871d3b646644c7fafd817585a18f
Matthew Elmer <>
2024-03-06 15:32:34 -0600

    implement color scheme generator

---
+++ src/PGFColorSchemes.jl
@@ -0,0 +1,42 @@
+module PGFColorSchemes
+export generate_color_definitions
+
+using ColorSchemes: colorschemes
+using PGFPlotsX: TikzPicture, pgfsave, CUSTOM_PREAMBLE
+
+function generate_color_definitions(cscheme::Symbol)
+    empty!(CUSTOM_PREAMBLE)
+    mkpath("generated_schemes")
+    palette = colorschemes[cscheme]
+    for i in 1:length(palette)
+        push!(CUSTOM_PREAMBLE,
+            "\\definecolor{$(string(cscheme))_$(i)}{rgb}{$(palette[i].r), " *
+            "$(palette[i].g), $(palette[i].b)}"
+        )
+    end
+    push!(CUSTOM_PREAMBLE,
+        """\\pgfplotscreateplotcyclelist{$(string(cscheme))}{
+        $(join(
+            ["    {$(string(cscheme))_$i}" for i in 1:length(palette)],
+            ",\n")
+        )
+        }"""
+    )
+    dummy_picture = TikzPicture()
+    generated_scheme_path = "generated_schemes/$(string(cscheme)).tex"
+    pgfsave(generated_scheme_path, dummy_picture)
+
+    tmppath, tmpfile = mktemp()
+    open(generated_scheme_path) do fin
+        lines = readlines(fin, keep=true)[13:(end-4)]
+        for line in lines
+            write(tmpfile, line)
+        end
+    end
+    close(tmpfile)
+    mv(tmppath, generated_scheme_path, force=true)
+
+    return nothing
+end
+
+end
