4743809f0d665187d96a55c25b5e55d815987a21
Matthew Elmer <>
2024-03-12 12:04:00 -0500

    make a buncha schemes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using PGFColorSchemes
 
-for cscheme in [:tab10, :managua10, :glasgow10, :hawaii10, :lipari10, :roma10, :Set2_8, :Set3_10, :Spectral, :tableau_10, :tableau_20]
+for cscheme in [
+    :tab10,  # The classic
+    :managua10,
+    :glasgow10,
+    :hawaii10,
+    :lipari10,
+    :roma10,
+    :Set2_8,
+    :Set3_10,
+    :Spectral,  # Beautiful
+    :tableau_10,  # A nice variation on the classic
+    :tableau_20,
+    :Paired_12  # Half are great for dark plots, half are great for light plots
+]
     generate_color_definitions(cscheme)
 end
