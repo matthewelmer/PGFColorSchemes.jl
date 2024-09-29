75b84677d4ed544e40795beff7e2b4b2dd188a11
Matthew Elmer <>
2024-09-29 10:48:28 -0500

    simplify code and get rid of PGFPlotsX dep

--- src/PGFColorSchemes.jl
+++ src/PGFColorSchemes.jl
@@ -2,41 +2,22 @@ module PGFColorSchemes
 export generate_color_definitions, colorschemes
 
 using ColorSchemes: colorschemes
-using PGFPlotsX: TikzPicture, pgfsave, CUSTOM_PREAMBLE
 
 function generate_color_definitions(cscheme::Symbol)
-    empty!(CUSTOM_PREAMBLE)
     mkpath("generated_schemes")
+    generated_scheme_path = joinpath("generated_schemes", "$(string(cscheme)).tex")
     palette = colorschemes[cscheme]
-    for i in 1:length(palette)
-        push!(CUSTOM_PREAMBLE,
-            "\\definecolor{$(string(cscheme))_$(i)}{rgb}{$(palette[i].r), " *
-            "$(palette[i].g), $(palette[i].b)}"
-        )
+    fout = open(generated_scheme_path, "w")
+    for ii in 1:length(palette)
+        write(fout, "\\definecolor{$(string(cscheme))_$(ii)}{rgb}{$(palette[ii].r), $(palette[ii].g), $(palette[ii].b)}\n")
     end
-    push!(CUSTOM_PREAMBLE,
-        """\\pgfplotscreateplotcyclelist{$(string(cscheme))}{
-        $(join(
-            ["    {$(string(cscheme))_$i}" for i in 1:length(palette)],
-            ",\n")
-        )
-        }"""
-    )
-    dummy_picture = TikzPicture()
-    generated_scheme_path = "generated_schemes/$(string(cscheme)).tex"
-    pgfsave(generated_scheme_path, dummy_picture)
-
-    tmppath, tmpfile = mktemp()
-    open(generated_scheme_path) do fin
-        lines = readlines(fin, keep=true)[13:(end-4)]
-        for line in lines
-            write(tmpfile, line)
-        end
+    write(fout, "\\pgfplotscreateplotcyclelist{$(string(cscheme))}{\n")
+    for ii in 1:length(palette)
+        write(fout, "    {$(string(cscheme))_$ii}\n")
     end
-    close(tmpfile)
-    mv(tmppath, generated_scheme_path, force=true)
+    write(fout, "}\n")
 
-    return nothing
+    return
 end
 
 end
